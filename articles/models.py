5243090c186811072f8497e252d38e300356832b
NigelNk <nigelnkambule32@gmail.com>
2026-01-04 06:40:20 +0200

    Add articleForm & Article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
+from ckeditor.fields import RichTextField
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='articles')
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = RichTextField()
+    image = models.ImageField(upload_to='articles')
