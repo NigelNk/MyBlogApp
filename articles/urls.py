968ea82561343637bbf282997092adfd1c06aa61
NigelNk <nigelnkambule32@gmail.com>
2026-01-05 11:42:22 +0200

    Add article create view

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CreateArticle
+from .views import CreateArticle, ArticleView
 
 urlpatterns = [
     path('create_article/', CreateArticle.as_view(), name='create_article'),
+    path('articles/', ArticleView.as_view(), name='articles'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,25 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, ListView
 
+from .forms import ArticleForm
 from .models import Article
 
 
 class CreateArticle(LoginRequiredMixin, CreateView):
-    model = Article
-    fields = ['title', 'content', 'image']
+    form_class = ArticleForm
     template_name = 'articles/create_article.html'
+    context_object_name = 'article'
+    login_url = reverse_lazy('login')
+    success_url = reverse_lazy('articles')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return  super().form_valid(form)
+
+class ArticleView(LoginRequiredMixin, ListView):
+    model = Article
+    context_object_name = 'articles'
+    template_name = 'articles/articles.html'
     login_url = reverse_lazy('login')
