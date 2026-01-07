4da3871108959a7264400681b80fe310717c5904
NigelNk <nigelnkambule32@gmail.com>
2026-01-07 17:57:58 +0200

    Add signup complete message

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView
@@ -10,3 +11,7 @@ class UserSignupView(CreateView):
     form_class = UserAccountCreationForm
     template_name = 'users/signup.html'
     success_url = reverse_lazy('login')
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Account created successfully, please login')
+        return  super().form_valid(form)
