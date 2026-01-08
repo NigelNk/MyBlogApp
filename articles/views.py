ca7e73addf3ccf2c7c71f387627d20e9c6686914
NigelNk <nigelnkambule32@gmail.com>
2026-01-08 15:36:05 +0200

    Add Article Delete View

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, DeleteView
 
 from .forms import ArticleForm
 from .models import Article
@@ -24,6 +26,7 @@ class PersonalArticleView(LoginRequiredMixin, ListView):
     context_object_name = 'articles'
     template_name = 'articles/personal_articles.html'
     login_url = reverse_lazy('login')
+
     ordering = ['created_at']
     paginate_by = 3
 
@@ -33,3 +36,22 @@ class ArticleDetail(LoginRequiredMixin, DetailView):
     template_name = 'articles/article_detail.html'
     context_object_name = 'article'
     login_url = reverse_lazy('login')
+
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+    model = Article
+    template_name = 'articles/article_delete.html'
+    success_url = reverse_lazy('personal_articles')
+    login_url = reverse_lazy('login')
+    context_object_name = 'article'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Article deleted successfully')
+        return super().form_valid(form)
