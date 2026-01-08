064834dd03b83635606ad793fe457c30dcc5f3a2
NigelNk <nigelnkambule32@gmail.com>
2026-01-08 15:40:30 +0200

    Fix bug

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, DeleteView
 
