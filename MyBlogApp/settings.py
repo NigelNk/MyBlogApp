0845a9a9d7c3554759f49df4cdb0aaccad9e506d
NigelNk <nigelnkambule32@gmail.com>
2026-01-29 06:45:12 +0200

    Fix bugs

--- MyBlogApp/settings.py
+++ MyBlogApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os.path
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,9 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-teb@p1j=+n0wsgz2*bnj6g(9wsu8dtqfp^k=bc+hh!dxv%x_8z'
 
-DEBUG = os.getenv("DEBUG", "False") == "True"
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["*"]  # fine for MVP
 
